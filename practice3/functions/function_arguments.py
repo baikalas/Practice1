522bbe163f506f586ef5a8e52545675bfb05a82e
baikalas <ulassapanov08@gmail.com>
2026-02-17 20:48:18 +0500

    lambda and functions

---
+++ practice3/functions/function_arguments.py
@@ -0,0 +1,12 @@
+def my_function(fname):
+  print(fname + " Refsnes")
+
+my_function("Emil")
+my_function("Tobias")
+my_function("Linus")
+
+def function(x, y):
+  return x + y
+
+result = function(5, 3)
+print(result)
