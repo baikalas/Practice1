d202ab706f0376d3c2b46b4ae8aa8b326dbb8282
baikalas <ulassapanov08@gmail.com>
2026-02-28 11:56:47 +0500

    da

--- practice4/json/exr.py
+++ practice4/json/exr.py
@@ -1,26 +1,22 @@
 import json
 
-with open('sample-data.json') as f: 
+with open('sample-data.json') as f:
     data = json.load(f)
 
 output = []
 output.append("Interface Status")
 output.append("=" * 80)
-output.append(f"{'DN':<50} {'Description':<20}  {'Speed':>7}  {'MTU':>6}")
+output.append(f"{'DN':<50} {'Description':<20}  {'Speed':>7}  {'MTU':>6}") #doing format like example output
 output.append(f"{'-'*50} {'-'*20}  {'-'*7}  {'-'*6}")
 
 for item in data['imdata']:
-    attrs = item['l1PhysIf']['attributes']
-    dn    = attrs.get('dn', '')
-    descr = attrs.get('descr', '')
-    speed = attrs.get('speed', '')
-    mtu   = attrs.get('mtu', '')
-    output.append(f"{dn:<50} {descr:<20}  {speed:>7}  {mtu:>6} ")
+    attrs = item['l1PhysIf']['attributes'] #attributes ишинен деректерді алу
+    dn    = attrs.get('dn', '') #dn іздеу
+    descr = attrs.get('descr', '') #description іздеу
+    speed = attrs.get('speed', '') #speed іздеу
+    mtu   = attrs.get('mtu', '') #mtu іздеу
+    output.append(f"{dn:<50} {descr:<20}  {speed:>7}  {mtu:>6} ") #шыққан нәрселерді форматқа келитіру
 
 
 result = '\n'.join(output)
 print(result)
-
-
-with open('interface_status.txt', 'w') as f:
-    f.write(result + '\n')
